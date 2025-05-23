0de1ee5648160b9d36559ea5a3677712a1bbe0b8
almac2022 <al@newgraphenvironment.com>
2025-05-23 12:48:23 -0700

    initial commit to close https://github.com/NewGraphEnvironment/water-temp-bc/issues/3

---
+++ scripts/sync-data.R
@@ -0,0 +1,12 @@
+#Sync entire data dir to s3-----------------------------------------------------------------------------------------------------
+processx::run(
+  'aws',
+  args = c('s3', 'sync', 'data', 's3://water-temp-bc/data'),
+  echo = TRUE,
+  spinner = TRUE,
+  timeout = 1200            # Timeout after 20 min
+)
+
+
+s3fs::s3_dir_ls("s3://water-temp-bc", recurse = TRUE)
+# s3fs::s3_dir_delete("s3://water-temp-bc/test")
