b63026684a32be0760a4a501bfda595f1d3d414c
almac2022 <al@newgraphenvironment.com>
2025-05-27 12:15:01 -0700

    add -delete flag so we mirror the repo in the bucket

--- scripts/sync-data.R
+++ scripts/sync-data.R
@@ -1,7 +1,14 @@
 #Sync entire data dir to s3-----------------------------------------------------------------------------------------------------
 processx::run(
   'aws',
-  args = c('s3', 'sync', 'data', 's3://water-temp-bc/data'),
+  args = c(
+    's3',
+    'sync',
+    'data',
+    's3://water-temp-bc/data',
+    # removes files that are not seen here locally
+    '--delete')
+  ,
   echo = TRUE,
   spinner = TRUE,
   timeout = 1200            # Timeout after 20 min
@@ -9,4 +16,5 @@ processx::run(
 
 
 s3fs::s3_dir_ls("s3://water-temp-bc", recurse = TRUE)
+s3fs::s3_dir_ls("s3://water-temp-bc/data", recurse = FALSE)
 # s3fs::s3_dir_delete("s3://water-temp-bc/test")
