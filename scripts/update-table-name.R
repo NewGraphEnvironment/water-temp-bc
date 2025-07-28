90f5b289c916c6fccb68a780d1d46b33323aa014
almac2022 <al@newgraphenvironment.com>
2025-07-28 01:29:48 -0700

    restamp our realtime_raw.parquet with its latest date and update gitpages to point to it for sample query. Remove realtime_raw.parquet to close #10

---
+++ scripts/update-table-name.R
@@ -0,0 +1,22 @@
+# here we will restamp our realtime_raw.parquet with its latest date
+
+con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "data/water-temp-bc.duckdb", read_only = FALSE)
+
+
+# Get max(Date) where Code = 'TW'
+date_max <- DBI::dbGetQuery(con, "
+  SELECT MAX(Date) AS max_date
+  FROM realtime_raw
+  WHERE Code = 'TW'
+")$max_date
+
+# Format the date
+out_table <- paste0("realtime_raw_", format(date_max, "%Y%m%d"))
+out_file <- paste0("data/", "realtime_raw_", format(date_max, "%Y%m%d"), ".parquet")
+
+
+# Export the table
+DBI::dbExecute(con, glue::glue(
+  "COPY realtime_raw TO '{out_file}' (FORMAT PARQUET)"
+))
+
