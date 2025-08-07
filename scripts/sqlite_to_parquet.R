1adc35497487bcce165d6b6ce5b0f241eeacafcb
almac2022 <al@newgraphenvironment.com>
2025-08-06 20:35:23 -0700

    improve comments

--- scripts/sqlite_to_parquet.R
+++ scripts/sqlite_to_parquet.R
@@ -1,9 +1,10 @@
-# we are going to burn our sqlite table that we extracted 2024-01-19 to a parquet in data so it can go to the bucket
+# we have a historic table
+# we are going to burn the sqlite table that we extracted 2024-01-19 to a parquet in data so it can go to the bucket
+# with `scripts/sync-data.R`
 
 dir <- "/Users/airvine/Projects/data/temp_realtime.sqlite"
 conn <- readwritesqlite::rws_connect(dir)
 readwritesqlite::rws_list_tables(conn)
-
 
 dat_existing <- readwritesqlite::rws_read_table("temp_realtime", conn = conn) |>
   ngr::ngr_tidy_cols_rm_na()
@@ -12,7 +13,6 @@ dat_existing <- readwritesqlite::rws_read_table("temp_realtime", conn = conn) |>
 date_max <- max(dat_existing$Date)
 out_table <- paste0("realtime_raw_", format(date_max, "%Y%m%d"))
 out_file <- paste0("data/", "realtime_raw_", format(date_max, "%Y%m%d"), ".parquet")
-
 
 con <- DBI::dbConnect(duckdb::duckdb())
 # Register the data frame as a DuckDB table
