1481d563ef38045ead6ca448ab4bbf5fb5ac887d
almac2022 <al@newgraphenvironment.com>
2025-08-06 20:36:46 -0700

    just some test lines

--- scripts/update-temp-realtime.R
+++ scripts/update-temp-realtime.R
@@ -50,3 +50,55 @@ DBI::dbExecute(
   con,
   glue::glue("COPY {DBI::SQL(out_table)} TO '{out_file}' (FORMAT PARQUET)")
 )
+
+DBI::dbDisconnect(con)
+
+#append to main parquet-----------------------------------------------------------------------------------------------------
+# for each station in the main parquet get the Parameter and max(Date) so that we can scrape hydat
+# just for the realtime data we don't have yet.  We still scrape all the station options though
+# b/c some new ones may have come online - those get the default
+
+
+con <- DBI::dbConnect(duckdb::duckdb())
+
+# name the main parquet
+path <- "data/realtime_raw_20250521.parquet"
+
+
+query <- glue::glue(
+  "SELECT
+    STATION_NUMBER,
+    Parameter,
+    MIN(Date) AS min_date,
+    MAX(Date) AS max_date
+  FROM '{path}'
+  -- turn it off
+  -- WHERE Parameter = '5'
+  GROUP BY STATION_NUMBER, Parameter;
+"
+)
+
+res_stations_raw <- DBI::dbGetQuery(con, query)
+
+res_stations <- res_stations_raw |>
+  dplyr::mutate(
+    days_since = as.integer(lubridate::today() - as.Date(max_date))
+    )
+
+
+
+
+
+purrr::map2_dfr(
+  .x = res_stations$STATION_NUMBER,
+  .y = res_stations$days_since,
+  .f = ~ngr_hyd_realtime(
+    id_station = .x,
+    param_primary = res_stations$Parameter,
+    param_secondary = NULL,
+    days_back = .y
+  )
+)
+
+
+#test of append workflow-----------------------------------------------------------------------------------------------------
